94222d1d15829b37d09e253dcee13fbadbcc0835
Pavel Tseva <paveltseva@gmail.com>
2025-10-27 13:28:33 +0200

    Introduce test API for sending messages to users by WebSockets

--- src/main/java/com/crm/notification/notification_service/NotificationServiceApplication.java
+++ src/main/java/com/crm/notification/notification_service/NotificationServiceApplication.java
@@ -2,12 +2,14 @@ package com.crm.notification.notification_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.crm.sharedlib", "com.crm.notification.notification_service"})
 public class NotificationServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NotificationServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
 
 }
