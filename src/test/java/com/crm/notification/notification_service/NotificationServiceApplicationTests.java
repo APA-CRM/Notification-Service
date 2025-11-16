6583da43ca19d96da4f57ef3512f787e6a9ade4b
Pavel Tseva <paveltseva@gmail.com>
2025-11-16 20:54:22 +0200

    Create tests for the consumers

--- src/test/java/com/crm/notification/notification_service/NotificationServiceApplicationTests.java
+++ src/test/java/com/crm/notification/notification_service/NotificationServiceApplicationTests.java
@@ -1,10 +1,8 @@
 package com.crm.notification.notification_service;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class NotificationServiceApplicationTests {
+class NotificationServiceApplicationTests extends BaseIntegrationTest {
 
     @Test
     void contextLoads() {
---
+++ src/test/java/com/crm/notification/notification_service/consumer/PasswordSenderConsumerIntegrationTest.java
@@ -0,0 +1,37 @@
+package com.crm.notification.notification_service.consumer;
+
+import com.crm.notification.notification_service.BaseIntegrationTest;
+import com.crm.notification.notification_service.service.consumer.PasswordSenderConsumer;
+import com.crm.sharedlib.dto.amqp.SendPasswordByEmailEvent;
+import jakarta.mail.internet.MimeMessage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+class PasswordSenderConsumerIntegrationTest extends BaseIntegrationTest {
+
+    @MockitoBean
+    private JavaMailSender javaMailSender;
+
+    @Autowired
+    private PasswordSenderConsumer consumer;
+
+    @Test
+    @DisplayName("Verification of the method when sending a password to user by email expected success")
+    void sendPasswordToUserExpectedSuccess() {
+        SendPasswordByEmailEvent message = new SendPasswordByEmailEvent();
+        message.setEmail("test@local");
+        message.setPassword("123456");
+
+        Mockito.when(javaMailSender.createMimeMessage())
+                .thenReturn(Mockito.mock(MimeMessage.class));
+
+        consumer.sendPassword(message);
+
+        Mockito.verify(javaMailSender, Mockito.atLeastOnce()).send(Mockito.any(MimeMessage.class));
+    }
+
+}
